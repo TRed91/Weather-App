b3dd454ab94fd38b9e5cf3c6546bd7e7ce6cb7b9
Thomas Roth <thomas.roth@aon.at>
2024-06-08 15:47:55 +0200

    code async function to retrieve data
    and function to process Data and return an object
    with all necessary information.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import './style.css';
+import processData from './processData';
+
+async function getWeatherInfo(location) {
+
+    try {
+        const fetchData = await fetch(`https://api.weatherapi.com/v1/current.json?key=8172e47f317945cfa99140930240606&q=${location}`, 
+            { mode: 'cors'}
+        );
+        const data = await fetchData.json();
+        return data;
+
+    } catch (err) {
+        console.log(new Error(err));
+    }
+}
+
+const search = document.querySelector('.search-field');
+const searchBtn = document.querySelector('.search-btn');
+
+searchBtn.addEventListener('click', async () => {
+    const weather = getWeatherInfo(search.value);
+    const getData = weather.then(data => processData(data));
+    const dataObj = await getData;
+    console.log(dataObj);
+});
+
+
 
---
+++ src/processData.js
@@ -0,0 +1,16 @@
+export default function processData(data) {
+    const country = data.location.country;
+    const city = data.location.name;
+    const region = data.location.region;
+
+    const tempC = data.current.temp_c;
+    const tempF = data.current.temp_f;
+    const feelslikeC = data.current.feelslike_c;
+    const feelslikeF = data.current.feelslike_f;
+
+    const humidity = data.current.humidity;
+
+    const condition = data.current.condition.text;
+
+    return {country, city, region, tempC, tempF, feelslikeC, feelslikeF, humidity, condition};
+}
